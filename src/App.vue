0410eb2a8a06344d59c3262f1dfae70960d87c37
Peery, Stephen <stephen.peery@cbsinteractive.com>
2020-02-01 09:54:43 -0500

    adding calc

--- src/App.vue
+++ src/App.vue
@@ -6,51 +6,27 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Calculator/>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Calculator from './components/Calculator';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Calculator
   },
 
   data: () => ({
---
+++ src/components/Calculator.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Bit Calculator</v-toolbar-title>
+                <v-spacer />
+
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                    <v-text-field
+                        label="X"
+                        placeholder="0x00"
+                        outlined
+                    ></v-text-field>
+                    <v-text-field
+                        label="Y"
+                        placeholder="0x00"
+                        outlined
+                    ></v-text-field>
+                    <v-text-field
+                        label="Result"
+                        placeholder="0x00"
+                        outlined
+                        disabled
+                    ></v-text-field>
+
+                    <v-btn small>&lt;&lt;</v-btn>
+                    <v-btn small>&gt;&gt;</v-btn>
+                    <v-btn small>&amp;</v-btn>
+                    <v-btn small>^</v-btn>
+                    <v-btn small>~</v-btn>
+                    <v-btn small>|</v-btn>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Calculate</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+  }
+</script>
